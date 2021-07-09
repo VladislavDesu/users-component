2d7aaf38b01734ae2e21009a8e88d6824aaac571
Vladislav Gryadko <vgryadko.dev@gmail.com>
2021-07-09 12:44:34 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+   const [users, setUsers] = useState(null);
+
+   async function getUser() {
+      try {
+         const response = await axios.get(
+            "https://jsonplaceholder.typicode.com/users"
+         );
+         setUsers(response.data);
+      } catch (error) {
+         console.error(error);
+      }
+   }
+
+   useEffect(() => {
+      getUser();
+      console.log(users);
+   }, [users]);
+
    return <div className="App"></div>;
 }
 
